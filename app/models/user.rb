d7feeb5af812858d16762def229cf4846418405f
adamgillfillan <adam.gillfillan@vikings.berry.edu>
2014-02-08 14:42:40 -0500

    added email validation and uniquess and name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
+  validates :name, presence: true
+  REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: REGEX_EMAIL },
+            uniqueness: { case_sensitive: false } #ensures a unique, case-insesitive email
 end
