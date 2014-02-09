4d4228071e28347f9d3b0c19d9bd0ced45ef44fa
adamgillfillan <adam.gillfillan@vikings.berry.edu>
2014-02-09 13:19:25 -0500

    added sessions and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 BackchannelApp::Application.routes.draw do
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "log_out" => "sessions#destroy", :as => "log_out"
   resources :posts
-
+  resources :sessions
   resources :users
   root :to=> 'users#new'
 
