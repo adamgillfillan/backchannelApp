3a67ce8daf0a6b57bdae982dc72fe4e0f126c35b
Stephen Schroeder <sschroed@mail.umw.edu>
2014-02-12 15:11:32 -0500

    Edited sessions_controller test, fixed repo problems

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,18 @@
 require_relative '../spec_helper'
+require 'ruby-debug'
 describe SessionsController do
+  before :all do
+    @user = User.new :name=>'Name', :email=>'Email@site.com'
+    @user.password = 'password'
+    @user.save
+
+    @sessions_controller = SessionsController.new
+  end
 
   describe 'authenticating user' do
-    it 'should check the database for a match'
+    it 'should check the database for a match' do
+      @newUser = User.authenticate @user.email, @user.password
+      assert_equal @newUser.email, @user.email
+    end
   end
 end
